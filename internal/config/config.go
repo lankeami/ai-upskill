cb09bdf402a59f57fedd36b07f671ebcd95fb1ee
Jay Chinthrajah <lankeami@gmail.com>
2026-04-15 16:13:31 -0400

    feat: add config loading with Viper

---
+++ internal/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type RSSSource struct {
+	Name string `mapstructure:"name"`
+	URL  string `mapstructure:"url"`
+}
+
+type RedditConfig struct {
+	Subreddits []string `mapstructure:"subreddits"`
+}
+
+type HackerNewsConfig struct {
+	MinScore int `mapstructure:"min_score"`
+}
+
+type SourcesConfig struct {
+	Reddit     RedditConfig     `mapstructure:"reddit"`
+	HackerNews HackerNewsConfig `mapstructure:"hackernews"`
+	RSS        []RSSSource      `mapstructure:"rss"`
+}
+
+type DedupConfig struct {
+	LevenshteinThreshold float64 `mapstructure:"levenshtein_threshold"`
+}
+
+type OutputConfig struct {
+	Dir    string `mapstructure:"dir"`
+	Format string `mapstructure:"format"`
+}
+
+type Config struct {
+	Sources   SourcesConfig        `mapstructure:"sources"`
+	Keywords  map[string][]string  `mapstructure:"keywords"`
+	Companies map[string][]string  `mapstructure:"companies"`
+	Dedup     DedupConfig          `mapstructure:"dedup"`
+	Output    OutputConfig         `mapstructure:"output"`
+}
+
+func Load(path string) (*Config, error) {
+	viper.SetConfigFile(path)
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("reading config: %w", err)
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("unmarshalling config: %w", err)
+	}
+
+	return &cfg, nil
+}
